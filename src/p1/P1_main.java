d51a15c28dddc793406dfe9abdca0c39b81c2be1
Leo Bernsmann <leo@MacBook-Air-von-Leo.local>
2025-11-06 12:34:53 +0100

    Frist Commit

---
+++ src/p1/P1_main.java
@@ -0,0 +1,15 @@
+package p1;
+
+public class P1_main {
+	public static void main(String[] args) {
+		int i = 2;
+		
+		if ((i < 2) || ((i > 3) && (i < 5))) {
+			System.out.println("Gewonnen!");	
+			} else {
+				System.out.println("Verloren :(");
+			
+			}
+	}
+
+}
