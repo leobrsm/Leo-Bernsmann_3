a3d099ea6e3c316369faa85e62f996802b5957f7
Leo Bernsmann <leo@MacBook-Air-von-Leo.local>
2025-11-06 12:42:49 +0100

    zweiter commit

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -2,13 +2,16 @@ package p1;
 
 public class P1_main {
 	public static void main(String[] args) {
-		int i = 2;
+		int wuerfel = 6;
 		
-		if ((i < 2) || ((i > 3) && (i < 5))) {
-			System.out.println("Gewonnen!");	
-			} else {
-				System.out.println("Verloren :(");
-			
+		if ((wuerfel < 2) || ((wuerfel > 3) && (wuerfel < 5))) {
+			System.out.println("Glückwunsch du hast gewonnen! :)");	
+			}else if (wuerfel == 2){
+				System.out.println("Schade, du hast verloren. :(");
+			}else if (wuerfel == 5) {
+				System.out.println("Das Spiel endet unentschieden");
+			}else {
+				System.out.println("Es konnte kein Ausgang festgestellt werden. :/");
 			}
 	}
 
---
+++ src/p2/P2_main.java
@@ -0,0 +1,8 @@
+package p2;
+
+public class P2_main {
+	public static void main(String[] args) {
+		
+	}
+
+}
